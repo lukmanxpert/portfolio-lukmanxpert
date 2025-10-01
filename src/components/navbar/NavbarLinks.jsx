2f9c91fbe12b06e2821ea8179f5809a21d033faa
lukmanxpert <lukmanmiah2004@gmail.com>
2025-10-01 06:45:11 +0600

    fixed download resume issues

--- src/components/navbar/NavbarLinks.jsx
+++ src/components/navbar/NavbarLinks.jsx
@@ -11,23 +11,36 @@ const links = [
 const NavbarLinks = () => {
   return (
     <ul className="flex lg:flex-row sm:flex-col gap-6 text-white font-body lg:relative sm:absolute sm:top-[120%] text-center left-[50%] -translate-x-[50%] lg:text-md sm:text-xl sm:bg-cyan/30 backdrop-blur-lg lg:bg-black sm:w-full py-4">
-      {links.map((link, index) => {
-        return (
-          <li key={index} className="group">
-            <Link
-              spy={true}
-              smooth={true}
-              duration={500}
-              offset={-130}
-              to={link.section}
-              className="cursor-pointer text-white hover:text-cyan transition-all duration-500"
-            >
-              {link.link}
-            </Link>
-            <div className="mx-auto bg-cyan w-0 group-hover:w-full h-[1px] transition-all duration-500"></div>
-          </li>
-        );
-      })}
+      <>
+        {links.map((link, index) => {
+          return (
+            <li key={index} className="group">
+              <Link
+                spy={true}
+                smooth={true}
+                duration={500}
+                offset={-130}
+                to={link.section}
+                className="cursor-pointer text-white hover:text-cyan transition-all duration-500"
+              >
+                {link.link}
+              </Link>
+              <div className="mx-auto bg-cyan w-0 group-hover:w-full h-[1px] transition-all duration-500"></div>
+            </li>
+          );
+        })}
+        <li className="group md:hidden">
+          <a
+            offset={-130}
+            href="/resume.pdf"
+            download="Sheikh_Lukman_CV.pdf"
+            className="cursor-pointer text-white hover:text-cyan transition-all duration-500"
+          >
+            Download Resume
+          </a>
+          <div className="mx-auto bg-cyan w-0 group-hover:w-full h-[1px] transition-all duration-500"></div>
+        </li>
+      </>
     </ul>
   );
 };
